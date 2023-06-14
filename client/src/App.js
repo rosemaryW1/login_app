8c3e994906eeecd279ebbc264362ac8b1ae2a353
rosemaryW1 <rosemary.wakaba@starthmore.edu>
2023-06-14 18:21:00 +0300

    First committ

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
+
+const router = createBrowserRouter([
+    {
+    path: '/',  
+    element: <Username></Username>
+},
+{
+    path: '/register',  
+    element: <Register></Register>
+},
+{
+    path: '/password',  
+    element: <Password></Password>
+},
+{
+    path: '/profile',  
+    element: <Profile></Profile>
+},
+{
+    path: '/recovery',  
+    element: <Recovery></Recovery>
+},
+{
+    path: '/reset',  
+    element: <Reset></Reset>
+},
+{
+    path: '*',  
+    element: <PageNotFound></PageNotFound>
+},
+
+]);
+
+export default function App(){
+    return (
+        <main>
+            <RouterProvider router={router} />
+           
+        </main>
+    )
+}
