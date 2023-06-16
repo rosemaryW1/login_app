edf1a1fd1d78041229eaa6366894d33e78bbd061
rosemaryW1 <rosemary.wakaba@starthmore.edu>
2023-06-16 20:51:29 +0300

    Complete Front End

--- server/server.js
+++ server/server.js
@@ -1,2 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
+const app = express();
+
+/**middleware*/
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); //less hackers know about our stack
+
+
+const port =8080;
+
+/**HTTP request */
+app.get('/', (req, res) => {
+    res.status(201).json("Home Get Request");
+});
+
+/**Start Server */
+app.listen(port, () => {
+    console.log(`Server connected to http://localhost:${port}`);
+}
+);
